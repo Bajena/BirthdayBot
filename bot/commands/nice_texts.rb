2866e746724e221c88b7dcdc6e734efecb987ed0
Jan Bajena <bajena3@gmail.com>
2018-05-03 18:01:53 +0200

    Add more texts

--- bot/commands/nice_texts.rb
+++ bot/commands/nice_texts.rb
@@ -1,5 +1,12 @@
 module Commands
   NICE_TEXTS = [
-    "Jesteś super!"
+    "Jesteś super!",
+    "Kocham Cię najbardziej na świecie",
+    "A Cię rozjadę zaraz!",
+    "Bardzo ładnie dziś wyglądasz ;*",
+    "Miiś, kocham Cię bardzo, bardzo, bardzo",
+    "Choć tuuuu!!!!",
+    "Jesteś najpiękniejszą kobietą jaką widział świat!",
+    "Uwielbiam Cię",
   ].freeze
 end
